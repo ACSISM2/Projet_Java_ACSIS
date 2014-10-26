11b86312361ddb839329eb180b4169d9137c6e64
Mohamed <Mohamed@192.168.1.31>
2014-10-26 12:00:07 +0100

    Commit. 1) Alléger la methode ouvrir_fichier qui fessait ouvrir un fichier rdf et le lire puis vider la table si elle est déja remplie et rajouter le triples sujet predicat objet a la table => donc la methode Lire_ficheier_rdf qui prend en charge la lecture d'un fichier rdf et ajouter le triples a la table et la methode vider_table qui permet de vider la table. 2) Le boutton Parcourir est remplacé par le menu File/RDF

--- Projet_Java_Acsis/src/tuto/int_rdf.java
+++ Projet_Java_Acsis/src/tuto/int_rdf.java
@@ -1,29 +1,21 @@
 package tuto;
 
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JScrollPane;
-
 import net.miginfocom.swing.MigLayout;
-
-import javax.swing.JButton;
-
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.InputStream;
-
 import javax.swing.JFileChooser;
-import javax.swing.JTextField;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JMenu;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Property;
@@ -32,63 +24,62 @@ import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.hp.hpl.jena.util.FileManager;
-
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 @SuppressWarnings("serial")
 public class int_rdf extends JFrame {
-
 	private JPanel panel1;
-	private JTextField textField;
     private JTable table;
     private int i=0;
-
-	private void ouvrir_fichier(java.awt.event.ActionEvent evt)  {  
-	    // choix d'un fichier rdf 
-    	String inputFileName ;
-        JFileChooser chooser=new JFileChooser();
-        chooser.showOpenDialog(null);
-        File f=chooser.getSelectedFile();
-        String filename=f.getAbsolutePath();
-        inputFileName= filename;
-        
-        // vider la table 
-        i=table.getRowCount();
-        i--;
-        //System.out.print(j);
-        for(int p=0;p<=i;p++){
-          ((DefaultTableModel)table.getModel()).removeRow(0);
-        }
-       
-        textField.setText(filename);
-        
-        // créer un modèle vide
+    private Resource  subject;
+    private Property  predicate;
+    private RDFNode   object;
+    
+    // methode qui permet la lecture d'un fichier RDF et afficher le triples dans une table
+    private void lire_fichier_rdf (String inputFileName,JTable Table){
+    	// créer un modèle vide
      	Model model = ModelFactory.createDefaultModel();
-     		 
-     		 
      		 // utiliser le FileManager pour trouver le fichier d'entrée
      		 InputStream in = FileManager.get().open( inputFileName );
      		if (in == null) {
      		    throw new IllegalArgumentException(
-     		                                 "Fichier: " + inputFileName + " non trouvé");
-        
+     		                                 "Fichier: " + inputFileName + " non trouvé");    
         }                                       
-    	
 	    model.read(in, null);
 	    StmtIterator iter = model.listStatements();
-	 
 		while (iter.hasNext()) {
          Statement stmt      = iter.nextStatement(); // get next statement
-         Resource  subject   = stmt.getSubject();   // get the subject
-         Property  predicate = stmt.getPredicate(); // get the predicate
-         RDFNode   object    = stmt.getObject();    // get the object
-         javax.swing.table.DefaultTableModel mod = (javax.swing.table.DefaultTableModel) table.getModel();
-         mod.addRow(new Object[]{subject,predicate,object});
-      
+          subject   = stmt.getSubject();   // get the subject
+          predicate = stmt.getPredicate(); // get the predicate
+          object    = stmt.getObject();    // get the object
+          //remplissage de la table 
+          javax.swing.table.DefaultTableModel mod = (javax.swing.table.DefaultTableModel) Table.getModel();
+          mod.addRow(new Object[]{subject,predicate,object});
+    }
+    }
+    // ouvrir le fichier RDF 
+	private void ouvrir_fichier(java.awt.event.ActionEvent evt)  {  
+	    // choix d'un fichier rdf 
+    	String filename ;
+        JFileChooser chooser=new JFileChooser();
+        chooser.showOpenDialog(null);
+        File f=chooser.getSelectedFile();
+        filename=f.getAbsolutePath();
+        
+        // vider la table 
+        table=vider_table (table);
+        lire_fichier_rdf (filename,table);
 		}
-   }
-	
+	public JTable vider_table (JTable Table)  // vider une table
+	{
+		 i=Table.getRowCount();
+	        for(int p=1;p<=i;p++){
+	          ((DefaultTableModel)Table.getModel()).removeRow(0);
+	        }
+			return Table;
+	}
+	// creation de la Jframe Fenetre principale de l'interface 
 	public int_rdf() throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 916, 512);
@@ -97,8 +88,16 @@ public class int_rdf extends JFrame {
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
-		JMenu mnQuitter = new JMenu("File");
-		menuBar.add(mnQuitter);
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmRDF = new JMenuItem("RDF");
+		mntmRDF.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				ouvrir_fichier(evt);
+			}
+		});
+		mnFile.add(mntmRDF);
 		
 		JMenuItem mntmQuitter = new JMenuItem("Quitter");
 		mntmQuitter.addActionListener(new ActionListener() {
@@ -106,7 +105,7 @@ public class int_rdf extends JFrame {
 				System.exit(0);
 			}
 		});
-		mnQuitter.add(mntmQuitter);
+		mnFile.add(mntmQuitter);
 		
 		JMenu mnEdit = new JMenu("Edit");
 		menuBar.add(mnEdit);
@@ -118,25 +117,10 @@ public class int_rdf extends JFrame {
 		JPanel panel = new JPanel();
 		panel1.add(panel, "cell 0 1,grow");
 		panel.setLayout(null);
-		
-		textField = new JTextField();
-		textField.setBounds(39, 111, 340, 20);
-		panel.add(textField);
-		textField.setColumns(10);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(209, 15, 2, 2);
 		panel.add(scrollPane);
-		
-		JButton btnNewButton = new JButton("Parcourir");
-		btnNewButton.setBounds(389, 110, 113, 23);
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent evt) {
-				
-				ouvrir_fichier(evt);
-			}
-		});
-		panel.add(btnNewButton);
 		
 		JScrollPane scrollPane_2 = new JScrollPane();
 		scrollPane_2.setBounds(0, 148, 866, 241);
@@ -165,10 +149,9 @@ public class int_rdf extends JFrame {
 				return columnEditables[column];
 			}
 		});
-		scrollPane_2.setViewportView(table);
-		
-		
+		scrollPane_2.setViewportView(table);		
 	}
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {

cfda34f64030644839acae515a4f66d7e3729f23
hassen <hassen@10.42.28.191>
2014-11-03 14:26:48 +0100

    Ajout de la méthode actualiser au JTable

--- Projet_Java_Acsis/src/tuto/Traitement.java
+++ Projet_Java_Acsis/src/tuto/Traitement.java
@@ -1,36 +1,43 @@
 package tuto;
 
 import java.io.File;
-
 import javax.swing.JFileChooser;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 public class Traitement {
-	
-	
-	public void vider_Jtable (JTable Table)  // vider une table
+
+	public void vider_Jtable (JTable Table)  // vider la table
 	{
-		 int i=Table.getRowCount();
-	        for(int p=1;p<=i;p++){
-	          ((DefaultTableModel)Table.getModel()).removeRow(0);
-	        }
+		int i=Table.getRowCount();
+		for(int p=1;p<=i;p++){
+			((DefaultTableModel)Table.getModel()).removeRow(0);
+		}
 	}
-	
+	//////actualiser la table //////////
+	public void actualiser_table(JTable Table){
+		Interface.traite.vider_Jtable(Interface.table);
+		Myrdf.affichage_rdf_Jtable (Interface.model_bis,Table);
+		Interface.label.setText("");
+		Interface.textField.setText("");
+	}
+
+
+	@SuppressWarnings("deprecation")
 	public String ouvrir_fichier()  {  
-	    // choix d'un fichier rdf 
-		 
-    	String filename ;
-    	
-        JFileChooser chooser=new JFileChooser();
-        chooser.showOpenDialog(null);
-     
-        File f=chooser.getSelectedFile();
-        filename=f.getAbsolutePath();
-     
-       
-		  return filename;
-}
+		// choix d'un fichier rdf 
+
+		String filename ;
+
+		JFileChooser chooser=new JFileChooser("RDF");
+		chooser.showOpenDialog(null);
+
+		File f=chooser.getSelectedFile();
+		filename=f.getAbsolutePath();
+		Interface.label_1.setText(f.getName());
+		Interface.textField.enable();
+		return filename;
+	}
 
 
 }

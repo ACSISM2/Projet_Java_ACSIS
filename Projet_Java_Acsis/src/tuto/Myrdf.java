7ffcec7590ba9b0bc8cb592fbee006ab9c64fc5e
anis <anis@10.42.4.5>
2014-11-03 14:36:27 +0100

    Amelioration code RDF

--- Projet_Java_Acsis/src/tuto/Myrdf.java
+++ Projet_Java_Acsis/src/tuto/Myrdf.java
@@ -1,7 +1,6 @@
 package tuto;
 
 import java.io.InputStream;
-
 import javax.swing.JTable;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
@@ -14,45 +13,49 @@ import com.hp.hpl.jena.util.FileManager;
 
 public class Myrdf {
 
-	
-
 	//////////recupèrè un fichier rdf a partir de son path/////////// 
-		/////////////////////////////////////////////////////
-		
-			public void lire_fichier_rdf (String inputFileName){
-				
-			
-	    	// créer un modèle vide
-	     	Model model = ModelFactory.createDefaultModel();
-	     	// utiliser le FileManager pour trouver le fichier d'entrée
-	     	InputStream in = FileManager.get().open( inputFileName );
-	     			if (in == null) {
-	     				throw new IllegalArgumentException(
-	     						"Fichier: " + inputFileName + " non trouvé");    
-	     			}                                       
-	     			model.read(in, null);
-	     			affichage_rdf_Jtable(model,Interface.table);
-		}	
-		///////////// affichage du rdf dans la Jtable
-		
-		 public void affichage_rdf_Jtable (Model model,JTable Table)
-		    {
-			//on vide la table au moment de l'affiche
-			   Interface.traite.vider_Jtable(Interface.table);
-			 //--------------------------------------------------------------
-		    	 StmtIterator iter = model.listStatements();
-		 		while (iter.hasNext()) {
-		          Statement stmt      = iter.nextStatement();    // get next statement
-		          Resource subject   = stmt.getSubject();        // get the subject
-		          Property predicate = stmt.getPredicate();     // get the predicate
-		          RDFNode object    = stmt.getObject();        // get the object
-		           //remplissage de la table 
-		           javax.swing.table.DefaultTableModel mod = (javax.swing.table.DefaultTableModel) Table.getModel();
-		           mod.addRow(new Object[]{subject,predicate,object});
-		    }
-		 		}
-		 
+	/////////////////////////////////////////////////////
+
+	public void lire_fichier_rdf (String inputFileName){
+		Interface.label.setText("");
+		Interface.textField.setText("");
+		Model model = ModelFactory.createDefaultModel();
+		Interface.model_bis=model;
+		// créer un modèle vide
+		//Model model = ModelFactory.createDefaultModel();
+		// utiliser le FileManager pour trouver le fichier d'entrée
+		InputStream in = FileManager.get().open( inputFileName );
+		if (in == null) {
+			throw new IllegalArgumentException(
+					"Fichier: " + inputFileName + " non trouvé");    
+		}                                       
+		model.read(in, null);
+		affichage_rdf_Jtable(model,Interface.table);
+	}	
+
+
+	///////////// affichage du rdf dans la Jtable
+
+	public static void affichage_rdf_Jtable (Model model,JTable Table)
+	{
+		//on vide la table au moment de l'affiche
+		Interface.traite.vider_Jtable(Table);
+		//--------------------------------------------------------------
+		StmtIterator iter = model.listStatements();
+		while (iter.hasNext()) {
+			Statement stmt      = iter.nextStatement();    // get next statement
+			Resource subject   = stmt.getSubject();        // get the subject
+			Property predicate = stmt.getPredicate();     // get the predicate
+			RDFNode object    = stmt.getObject();        // get the object
+			//remplissage de la table 
+			javax.swing.table.DefaultTableModel mod = (javax.swing.table.DefaultTableModel) Table.getModel();
+			mod.addRow(new Object[]{subject,predicate,object});
+		}
 	}
 
-	
+
+
+}
+
+
 
